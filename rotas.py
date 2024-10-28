886f79551168fbae3272b1a50809079ec5fb672a
Yuri-Fernandes-dev <devyuri.programador@gmail.com>
2024-10-27 21:33:38 -0300

    Commit

---
+++ rotas.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/pdv")
+def menu():
+    return render_template('menu.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
